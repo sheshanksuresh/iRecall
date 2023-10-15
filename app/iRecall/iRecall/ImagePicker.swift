d5f75c33e4358e38fd56a3773f7de73368477e3c
Sheshank Suresh <sheshanknair@gmail.com>
2023-10-15 15:51:31 -0400

    created registration view UI

---
+++ app/iRecall/iRecall/ImagePicker.swift
@@ -0,0 +1,8 @@
+//
+//  ImagePicker.swift
+//  iRecall
+//
+//  Created by Sheshank Suresh on 2023-10-15.
+//
+
+import Foundation
---
+++ app/iRecall/iRecall/RegistrationView.swift
@@ -0,0 +1,8 @@
+//
+//  RegistrationView.swift
+//  iRecall
+//
+//  Created by Sheshank Suresh on 2023-10-15.
+//
+
+import Foundation
