595b2e628b2dfa4648c712870fcda9b0d06bc0de
Sheshank Suresh <sheshanknair@gmail.com>
2023-10-14 23:04:18 -0400

    begain initial login/register screen

--- app/iRecall/iRecall/ContentView.swift
+++ app/iRecall/iRecall/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var isLoginView: Bool = true
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            VStack {
+                TextField("Username", text: $username)
+                    .padding()
+                    .background(Color(.secondarySystemFill))
+                    .cornerRadius(8)
+                SecureField("Password", text: $password)
+                    .padding()
+                    .background(Color(.secondarySystemFill))
+                    .cornerRadius(8)
+                
+                if isLoginView {
+                    Button("Login"){
+                        // TODO: Login Logic
+                    }.padding()
+                    
+                    Button("Switch to Register") {
+                        isLoginView.toggle()
+                    }.padding()
+                } else {
+                    Button("Register") {
+                        isLoginView.toggle()
+                    }.padding()
+                }
+            }
         }
-        .padding()
     }
 }
 
---
+++ app/iRecall/iRecall/NetworkManager.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkManager.swift
+//  iRecall
+//
+//  Created by Sheshank Suresh on 2023-10-14.
+//
+
+import Foundation
+
+struct AuthResponse: Codable {
+    let token: String
+}
+
+//TODO: Logic to verify login from Django backend
+func loginUser(username: String, password: String, completion: @escaping (AuthResponse?) -> Void) {
+    guard let url = URL(string: baseURL + "login/") else { return }
+    
+}
